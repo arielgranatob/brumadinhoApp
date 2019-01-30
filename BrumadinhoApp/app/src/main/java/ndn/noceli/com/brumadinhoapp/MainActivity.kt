0670466509051f0a52e0f20a8593250125643d4d
Diego <diegonoceli>
2019-01-29 22:19:36 -0200

    Falta verificação de campo vazio e alerta quando for cadastrado!

--- BrumadinhoApp/app/src/main/java/ndn/noceli/com/brumadinhoapp/MainActivity.kt
+++ BrumadinhoApp/app/src/main/java/ndn/noceli/com/brumadinhoapp/MainActivity.kt
@@ -4,12 +4,16 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCenter.start(application, "82995e17-ffd7-45f9-96ae-625a6a0e7c66", Analytics::class.java, Crashes::class.java)
     }
 
     override fun onStart() {
