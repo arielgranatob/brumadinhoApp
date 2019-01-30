de021fe5e21a02b31d050cb08f819d0db70847c9
Guilherme Rocha <guilherme@candiba.net>
2019-01-30 17:39:45 -0200

    Valida campos obrigatorios e imagens

--- cadastroDesaparecidos.php
+++ cadastroDesaparecidos.php
@@ -2,39 +2,59 @@
 
 include 'conexao.php';
 
-$nomeDesaparecido = $_POST["nomeDesaparecido"];
-$fotoDesaparecido = $_POST["fotoDesaparecido"];
-$celularDesaparecido = $_POST["celularDesaparecido"];
-$caractFisicas = $_POST["caractFisicas"];
-$tatuagemDesaparecido = $_POST["tatuagemDesaparecido"];
-$fotoTatuagem = $_POST["fotoTatuagem"];
-$cicatrizDesaparecido = $_POST["cicatrizDesaparecido"];
-$regiaoCicatriz = $_POST["regiaoCicatriz"];
-$caractMarcante = $_POST["caractMarcante"];
-$nomeDeclarante = $_POST["nomeDeclarante"];
-$contatoDeclarante = $_POST["contatoDeclarante"];
+// getPost(<nome_do_campo>,<eh_obrigatorio>)
+function getPost($campo,$obrigat) {
+	$dado = filter_input ( 'INPUT_POST' , $campo, FILTER_SANITIZE_SPECIAL_CHARS);
+	if(is_null($dado) && $obrigat === true ? $vazio[] = $campo : null );
+	return $dado;
+}
+
+// getImage(<foto/imagem>)
+function getImage($foto) {
+	if(is_null($foto)) {
+		return null;
+	}
+	else {
+		$filename_path = md5(time().uniqid()).".jpg";
+		$decoded=base64_decode($foto); 
+		file_put_contents("images/".$filename_path,$decoded); 
+		return $filename_path;
+	}
+}
+
+$nomeDesaparecido = getPost("nomeDesaparecido",true);
+$fotoDesaparecido = getPost("fotoDesaparecido",false);
+$celularDesaparecido = getPost("celularDesaparecido",true);
+$caractFisicas = getPost("caractFisicas",true);
+$tatuagemDesaparecido = getPost("tatuagemDesaparecido",false);
+$fotoTatuagem = getPost("fotoTatuagem",false);
+$cicatrizDesaparecido = getPost("cicatrizDesaparecido",false);
+$regiaoCicatriz = getPost("regiaoCicatriz",false);
+$caractMarcante = getPost("caractMarcante",false);
+$nomeDeclarante = getPost("nomeDeclarante",true);
+$contatoDeclarante = getPost("contatoDeclarante",true);
+
+$actualpath1=getImage($fotoDesaparecido);
+$actualpath2=getImage($fotoTatuagem);
 
 
-$filename_path = md5(time().uniqid()).".jpg";
-$decoded=base64_decode($fotoDesaparecido); 
-file_put_contents("images/".$filename_path,$decoded); 
-$actualpath1=$filename_path;
+// stat com retorno 0 = erro ; retorno = 1 sucesso; 
+if (!is_null($vazio)) {
+	$erro = array('stat' => 0 , 'msg' => 'Campos obrigatórios vazios', 'fields' => $vazio );
+	echo json_encode($erro);
+}
+else {
+	$sql = "INSERT INTO `tblpessoa`(`nomePessoa`, `fotoPessoa`, `celularPessoa`, `caracteristicasPessoa`, `tatuagemPessoa`, `cicatrizPessoa`, `fototatuagemPessoa`, `obscicatrizPessoa`, `caractmarcantesPessoa`, `nomedodeclarantePessoa`, `contatodeclarantePessoa`) 
+		VALUES
+		('".$nomeDesaparecido."','".$actualpath1."','".$celularDesaparecido."',
+		'".$caractFisicas."','".$tatuagemDesaparecido."','".$cicatrizDesaparecido."','".$actualpath2."',
+		'".$regiaoCicatriz."','".$caractMarcante."','".$nomeDeclarante."','".$contatoDeclarante."')" ; 
 
+		if ($con->query($sql) == TRUE) {
+			$result = array('stat' => 1, 'msg' => 'Cadastro com sucesso!');
+		} else {
+			$result = array('stat' => 0, 'msg' => "Erro: " . $sql . " - " . $conn->error);
+		}
 
-$filename_path = md5(time().uniqid()).".jpg";
-$decoded=base64_decode($fotoTatuagem); 
-file_put_contents("images/".$filename_path,$decoded); 
-$actualpath2=$filename_path;
-
-
-$sql = "INSERT INTO `tblpessoa`(`nomePessoa`, `fotoPessoa`, `celularPessoa`, `caracteristicasPessoa`, `tatuagemPessoa`, `cicatrizPessoa`, `fototatuagemPessoa`, `obscicatrizPessoa`, `caractmarcantesPessoa`, `nomedodeclarantePessoa`, `contatodeclarantePessoa`) 
-VALUES
-('".$nomeDesaparecido."','".$actualpath1."','".$celularDesaparecido."',
-'".$caractFisicas."','".$tatuagemDesaparecido."','".$cicatrizDesaparecido."','".$actualpath2."',
-'".$regiaoCicatriz."','".$caractMarcante."','".$nomeDeclarante."','".$contatoDeclarante."')" ; 
-
-if ($con->query($sql) == TRUE) {
-	echo "Cadastrado com sucesso!";
-} else {
-	echo "Erro: " . $sql . "<br>" . $conn->error;
+		echo json_encode($result);
 }
